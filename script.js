bbe7c8de2da4f6e846513ca3a7bc883a0118373d
Tshino118 <t.shinohara991108@gmail.com>
2026-08-25 12:56:29 +0900

    Refine response to record profits

--- script.js
+++ script.js
@@ -69,5 +69,5 @@ buttons.forEach((button) => {
   });
 });
 
-loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-11`, storyMarkup);
-loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-11`, notesMarkup);
+loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-12`, storyMarkup);
+loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-12`, notesMarkup);
