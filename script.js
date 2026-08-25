0589b36415125f05008f16a5e9348b56526920e8
Tshino118 <t.shinohara991108@gmail.com>
2026-08-25 12:43:08 +0900

    Delay reveal that Zero remains with Arai

--- script.js
+++ script.js
@@ -69,5 +69,5 @@ buttons.forEach((button) => {
   });
 });
 
-loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-8`, storyMarkup);
-loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-8`, notesMarkup);
+loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-9`, storyMarkup);
+loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-9`, notesMarkup);
