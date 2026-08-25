b4ac1ac59db5f01930ed83951839ac023decdf07
Tshino118 <t.shinohara991108@gmail.com>
2026-08-25 12:40:03 +0900

    Give acquisition recommendation to Zero

--- script.js
+++ script.js
@@ -69,5 +69,5 @@ buttons.forEach((button) => {
   });
 });
 
-loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-5`, storyMarkup);
-loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-5`, notesMarkup);
+loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-6`, storyMarkup);
+loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-6`, notesMarkup);
