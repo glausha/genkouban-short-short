094ece091ce9abbc0f85bcb51c1de85719548c08
Tshino118 <t.shinohara991108@gmail.com>
2026-08-25 12:41:16 +0900

    Retain Zero through acquisition

--- script.js
+++ script.js
@@ -69,5 +69,5 @@ buttons.forEach((button) => {
   });
 });
 
-loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-6`, storyMarkup);
-loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-6`, notesMarkup);
+loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-7`, storyMarkup);
+loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-7`, notesMarkup);
