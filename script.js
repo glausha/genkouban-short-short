f0f4b1b59803afff85dbd660cb4162d6f053bae9
Tshino118 <t.shinohara991108@gmail.com>
2026-08-25 12:33:08 +0900

    Clarify competing robot launch

--- script.js
+++ script.js
@@ -69,5 +69,5 @@ buttons.forEach((button) => {
   });
 });
 
-loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=6c22744`, storyMarkup);
-loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=6c22744`, notesMarkup);
+loadText(document.querySelector('#story'), `${encodeURI('現行版_v3.txt')}?v=v3-2`, storyMarkup);
+loadText(document.querySelector('#notes'), `${encodeURI('現行版_v3.txt.read')}?v=v3-2`, notesMarkup);
